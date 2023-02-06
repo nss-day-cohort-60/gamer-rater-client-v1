fad9f4d4378c098a0f27d2ff73ef479e3d31d474
Steve <chortlehoort@gmail.com>
2023-02-06 10:21:20 -0600

    Removed references to Level Up and added email field for registration

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -36,7 +36,7 @@ export const Login = () => {
             </dialog>
             <section>
                 <form className="form--login" onSubmit={handleLogin}>
-                    <h1>Level Up</h1>
+                    <h1>Gamer Rater</h1>
                     <h2>Please sign in</h2>
                     <fieldset>
                         <label htmlFor="inputUsername"> Username address </label>
